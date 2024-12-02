2c9039d09d73a194e050a48adfcee08ff5564d72
thiagoyc <thiagoyc@gmail.com>
2024-12-02 14:28:00 -0300

    Mudanças ultima aula

--- Assets/Scripts/DictationHandler.cs
+++ Assets/Scripts/DictationHandler.cs
@@ -5,14 +5,45 @@ using Meta.WitAi.Dictation;
 using Oculus.Voice;
 using Oculus.Voice.Dictation;
 using TMPro;
-using static System.Net.Mime.MediaTypeNames;
 
 public class DictationHandler : MonoBehaviour
 {
     [Header("Wit Configuration")]
     [SerializeField] private AppDictationExperience appVoiceExperience;
-    public bool active;
-    public TextMeshProUGUI transcript;
+    private bool active;
+    public TextMeshProUGUI transcriptDebug;
+    public TextMeshProUGUI transcriptFull;
+    private string lastTranscription;
+    private int currentPage;
+    private int maxPage;
+    public TextMeshProUGUI pageString;
+    private int transcriptionCount;
+
+    public void Start()
+    {
+        if (appVoiceExperience != null)
+        {
+            appVoiceExperience.Deactivate(); // Garante que está desativado ao iniciar
+        }
+        lastTranscription = string.Empty;
+        transcriptDebug.text = string.Empty;
+        transcriptFull.text = string.Empty;
+        active = false;
+        currentPage = 1;
+        transcriptionCount = 0;
+    }
+
+    public void OnDisable()
+    {
+        Microphone.End(null);
+        StopTranscription();
+    }
+
+    public void Update()
+    {
+        maxPage = transcriptFull.textInfo.pageCount;
+        pageString.text = currentPage.ToString() + "/" + maxPage.ToString();
+    }
 
     // Método público para iniciar a transcrição, chamado pelo botão de início
     public void StartTranscription()
@@ -25,6 +56,13 @@ public class DictationHandler : MonoBehaviour
 
         if (!active)
         {
+            if (lastTranscription != string.Empty)
+            {
+                transcriptFull.text += lastTranscription + "\n\n";
+                lastTranscription = string.Empty;
+            }
+            transcriptionCount++;
+            transcriptFull.text += "GRAVAÇÃO " + transcriptionCount.ToString() + "\n\n";
             active = true;
             appVoiceExperience.Activate();
             appVoiceExperience.DictationEvents.OnPartialTranscription.AddListener(OnPartialTranscription);
@@ -47,6 +85,41 @@ public class DictationHandler : MonoBehaviour
     private void OnPartialTranscription(string transcription)
     {
         Debug.Log(transcription);
-        transcript.text = transcription;
+        transcriptDebug.text = transcription;
+        if (!transcription.StartsWith(lastTranscription))
+        {
+            transcriptFull.text += lastTranscription + "\n";
+        }
+        lastTranscription = transcription;
+    }
+
+    public void NextPage()
+    {
+        if (transcriptFull.pageToDisplay < maxPage)
+        {
+            transcriptFull.pageToDisplay += 1;
+            currentPage = transcriptFull.pageToDisplay;
+        }
+    }
+
+    public void PreviousPage()
+    {
+        if (transcriptFull.pageToDisplay > 1)
+        {
+            transcriptFull.pageToDisplay -= 1;
+            currentPage = transcriptFull.pageToDisplay;
+        }
+    }
+
+    public void FirstPage()
+    {
+        transcriptFull.pageToDisplay = 1;
+        currentPage = transcriptFull.pageToDisplay;
+    }
+
+    public void LastPage()
+    {
+        transcriptFull.pageToDisplay = maxPage;
+        currentPage = transcriptFull.pageToDisplay;
     }
 }
